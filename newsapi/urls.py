813fd7a2ac89ac3f9c94a169f655ff03a83e1f47
haenno <haenno@web.de>
2023-09-10 20:16:42 +0200

    add create fake news

--- newsapi/urls.py
+++ newsapi/urls.py
@@ -5,23 +5,16 @@ from faker import Faker
 
 from newsapi.models import News
 from newsapi.views import (
+    CreateFakeNewsAPIView,
     CreateNewsAPIView,
     LatestNewsListAPIView,
     NewsAfterIdListAPIView,
 )
 
-# create 30 news on each startup
-# for _ in range(30):
-#    new_news = News()
-#    new_news.news_uuid = uuid.uuid4()
-#    new_news.assigned_to_uuid = uuid.uuid4()
-#    new_news.news_text = Faker().text(max_nb_chars=300)
-#    new_news.save()
-
-
 app_name = "newsapi"
 
 urlpatterns = [
+    path("fake_news/", CreateFakeNewsAPIView.as_view()),
     path("create_news/", CreateNewsAPIView.as_view(), name="create_news"),
     path("latest_news/", LatestNewsListAPIView.as_view(), name="latest_news"),
     path(
--- newsapi/views.py
+++ newsapi/views.py
@@ -3,9 +3,37 @@ import uuid
 
 from faker import Faker
 from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from newsapi.models import News
 from newsapi.serializers import NewsSerializer
+
+
+class CreateFakeNewsAPIView(APIView):
+    def get_queryset(self):
+        return News.objects.all()
+
+    def get_serializer(self, *args, **kwargs):
+        return NewsSerializer(*args, **kwargs)
+
+    def get(self, request, format=None):
+        new_news = News()
+        new_news.news_uuid = uuid.uuid4()
+        new_news.assigned_to_uuid = uuid.uuid4()
+        new_news.news_text = Faker().text(max_nb_chars=300)
+        new_news.save()
+        saved_news = self.get_queryset().filter(pk=new_news.pk).first()
+        return Response(
+            {
+                "id": saved_news.pk,
+                "news_uuid": saved_news.news_uuid,
+                "created": saved_news.created,
+                "assigned_to_uuid": saved_news.assigned_to_uuid,
+                "news_text": saved_news.news_text,
+            },
+            status=201,
+        )
 
 
 class CreateNewsAPIView(CreateAPIView):
